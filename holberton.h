3708e9f8aef34635644e7b70d4bcc76c785f6989
Hugo Vargas <vargas88hugo@gmail.com>
2019-07-28 23:39:12 +0000

    [fix] #5 prototypes functions added and fixed the guard

--- holberton.h
+++ holberton.h
@@ -1,10 +1,16 @@
 #ifndef MY_PRINTF
 #define MY_PRINTF
+#include <stdarg.h>
+
+typedef struct pf
+{
+	char a;
+	void (*f)(va_list);
+} st;
 
 int _printf(const char *format, ...);
-
 int _putchar(char c);
-
-int p_char(va_list arg);
+void fnchar(va_list arg);
+void fnstring(va_list arg);
 
 #endif /* MY_PRINTF */
