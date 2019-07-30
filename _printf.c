a95e1d08c2e3fc8dd80c2d96c099ae119c3b14c7
alejogonza <alejo.1996.2001@gmail.com>
2019-07-30 20:51:42 +0000

    [fix] #1 betty style

--- _printf.c
+++ _printf.c
@@ -9,25 +9,25 @@
  */
 int (*fnstruct(char format))(va_list)
 {
-        int i;
+	int i;
 
-        st arr[] = {
-                {'c', fnchar},
-                {'s', fnstring},
-                {'m', fnsucc},
-                {'d', fnint},
-                {'i', fnint},
-                {'b', fnbin},
-                {'\0', fnerror}
-        };
+	st arr[] = {
+		{'c', fnchar},
+		{'s', fnstring},
+		{'m', fnsucc},
+		{'d', fnint},
+		{'i', fnint},
+		{'b', fnbin},
+		{'\0', fnerror}
+	};
 
-        for (i = 0; arr[i].a != '\0'; i++)
-        {
-                if (format == arr[i].a)
-                        return (arr[i].f);
-        }
+	for (i = 0; arr[i].a != '\0'; i++)
+		{
+		if (format == arr[i].a)
+		return (arr[i].f);
+		}
 
-        return (fnerror);
+	return (fnerror);
 }
 
 /**
@@ -38,39 +38,39 @@ int (*fnstruct(char format))(va_list)
  */
 int _printf(const char *format, ...)
 {
-        long int i, count = 0;
-        va_list arg;
+	long int i, count = 0;
+	va_list arg;
 
-        if (format == NULL)
-                return (-1);
+	if (format == NULL)
+		return (-1);
 
-        va_start(arg, format);
-        for (i = 0; format && format[i] != '\0'; i++)
-        {
-                if (format[i] == '%')
-                {
-                        if (format[i + 1] == '\0')
-                                return (-1);
-                        else if (format[i + 1] == '%')
-                        {
-                                _putchar('%'), i++, count++;
-                        }
-                        else
-                        {
-                                count += fnstruct(format[i + 1])(arg);
-                                i++;
+	va_start(arg, format);
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '\0')
+				return (-1);
+			else if (format[i + 1] == '%')
+			{
+				_putchar('%'), i++, count++;
+			}
+			else
+			{
+				count += fnstruct(format[i + 1])(arg);
+				i++;
 
-                                if (count == -1)
-                                {
-                                        _putchar('%');
-                                        _putchar(format[i]);
-                                        count = 2;
-                                }
-                        }
-                }
-                else
-                        _putchar(format[i]), count++;
-        }
-        va_end(arg);
-        return (count);
+				if (count == -1)
+				{
+					_putchar('%');
+					_putchar(format[i]);
+					count = 2;
+				}
+			}
+		}
+		else
+			_putchar(format[i]), count++;
+	}
+	va_end(arg);
+	return (count);
 }
