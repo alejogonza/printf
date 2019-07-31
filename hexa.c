965f5a5849aee775a0b749de6bca089dce646f53
alejogonza <alejo.1996.2001@gmail.com>
2019-07-31 17:34:09 +0000

    [update] advanced task

---
+++ hexa.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+ * hexadec - conversor hex to dec
+ * @var: int
+ * Return: 0
+ */
+
+int hexadec(va_list hexvaric)
+{
+	int num1, num2, temp, conv[100];
+	unsigned int count;
+
+	count = va_arg(hexvaric, unsigned int);
+	temp = 0;
+	num = 0;
+
+	if ( count == 0)
+	{
+	 _putchar('0');
+	return 
+	}
+	
+
+}
