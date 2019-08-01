e81dc94c260dd1648e19a93645848d7aa7c18d70
Hugo Vargas <vargas88hugo@gmail.com>
2019-08-01 02:14:19 +0000

    [fix] binary fix

--- advance.c
+++ advance.c
@@ -9,28 +9,30 @@
  */
 int fnbin(va_list arg)
 {
-	int i, j, n, count = 0;
-
+	unsigned int i, j, n, count = 0;
 	unsigned int arr[100];
-
-	n = va_arg(arg, unsigned int);
-
-	if (n == 0)
+	n = va_arg(arg, int);
+	if (n < 1)
 	{
 		_putchar('0');
 		return (1);
 	}
-	for (i = 0; n > 0; i++)
+	else
 	{
+		for (i = 0; n > 0; i++)
+		{
+			arr[i] = n % 2;
+			n /= 2;
+		}
 		arr[i] = n % 2;
-		n /= 2;
-	}
-	for (j = i - 1; j >= 0; j--)
-	{
-		_putchar(arr[j] + 48);
+		for (j = i - 1; j > 0; j--)
+		{
+			_putchar(arr[j] + 48);
+			count++;
+		}
 		count++;
+		_putchar(arr[j] + 48);
 	}
-
 	return (count);
 }
 
